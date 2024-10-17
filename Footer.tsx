30f4e2ca4863be3ef1973294f017ae85fffff3b8
farah-naz123 <fnaz29103@gmail.com>
2024-10-17 09:19:16 +0500

    Add files via upload

---
+++ Footer.tsx
@@ -0,0 +1,8 @@
+import cssStyle from "./footer.module.css";
+
+const Footer=() => {
+    return(
+        <div className={cssStyle.h1}>This is footer component</div>
+    )
+};
+export default Footer;
