20cbecc0e4d57b4623dc19b7cdf0f71fabdf0ec3
farah-naz123 <fnaz29103@gmail.com>
2024-10-17 09:18:16 +0500

    Add files via upload

---
+++ hero.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import style from "./hero.module.css"
+
+const Hero=() => {
+    return(
+        <div>
+            <h1 className={style.container}>This is hero section</h1>
+        </div>
+    )
+};
+export default Hero;
