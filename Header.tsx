892079dcbb18e37847a1ab865a9538ab44d50bb8
farah-naz123 <fnaz29103@gmail.com>
2024-10-17 09:18:45 +0500

    Add files via upload

---
+++ Header.tsx
@@ -0,0 +1,11 @@
+import cssStyle from "./header.module.css"
+
+const Header=() => {
+    return(
+    <div>
+        <h1 className={cssStyle.h1}>This is header component</h1>
+    </div> 
+    )
+};
+export default Header;
+
